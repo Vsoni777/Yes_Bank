775f7bb25940ad29efbb30cb1527559c2b569738
Vsoni777 <viveksoni85963@gmail.com>
2024-12-16 19:38:00 +0530

    Perform main operation

---
+++ Main.rb
@@ -0,0 +1,24 @@
+class Main
+  def initialize
+    
+  end
+  #to get Account_holder detail
+  def account_holder
+    
+  end
+
+  #Perform operation on using accountholder,operation(credit,debit,display)
+  def operation(acc,operation)
+    
+  end
+
+  #push object in this array
+  def register
+    
+  end
+
+  #check user is authenticate or not
+  def authentication_user(acc)
+    
+  end
+end
