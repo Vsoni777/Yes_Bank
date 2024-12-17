73b9ae8a54661c42eafbeff5bc3adc6e6b1a207b
Vsoni777 <viveksoni85963@gmail.com>
2024-12-17 17:04:42 +0530

    to perform operation on Account

--- Operation.rb
+++ Operation.rb
@@ -1,21 +1,31 @@
 module Operation 
-   attr_accessor :amount
-  def credit(account_holder)
-    puts "current balance is: #{account_holder.balance}"
-    puts "Enter Amount of Balance"
+  
+   #Method for credit
+  def self.credit(account,op,accounts)
+    puts "current balance is: #{account[:balance]}"
+    puts "Enter Amount for credit"
     balance=gets.chomp.to_i
-    account_holder.balance=balance
-    account_holder
+    account[:balance]+=balance
+    return account
   end
 
-  def debit(account_holder)
-    puts "Current balance is : #{account_holder.balance}"
+  #method for debit
+  def self.debit(account,op,accounts)
+    puts "Current balance is : #{account[:balance]}"
     puts "Enter Debit amount: "
     amount=gets.chomp.to_i
-    account_holder.balance=amount
+    account[:balance]-=amount
+    return account
   end
 
-  def display(account_holder)
-    
+  #method for display current use
+  def self.display(account,op)
+    puts "Account no: #{account[:acc_no]}"
+    puts "Account no: #{account[:name]}"
+    puts "Account no: #{account[:ph_no]}"
+    puts "Account no: #{account[:acc_type]}"
+    puts "Account no: #{account[:balance]}"
+    puts "Account no: #{account[:email]}"
+    puts "Account no: #{account[:passsword]}"
   end
 end
