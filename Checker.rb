31b850582b83f2fb48d1975c3065408bf54c0caa
Vsoni777 <viveksoni85963@gmail.com>
2024-12-17 17:05:46 +0530

    use for valid or invalid logic implementation

--- Checker.rb
+++ Checker.rb
@@ -1,11 +1,23 @@
 module Checker 
-   attr_accessor :ph_no
-  def valid_input?(ph_no)
+   #check Phone_no valid or not
+   def self.valid?(ph_no)
+    ph_no.match?(/^(\(\d{3}\)\s?|\d{3}-)\d{3}-\d{4}$/)
+   end
+
+   #check login detail for authentication
+   def self.login(email,password,accounts)
+    matched=accounts.find do |account|
+      account[:email]==email && account[:passsword]==password
+    end
+    if matched
+      puts "login Successfully"
+      puts "You are logged in : #{matched[:name]}"
+      return matched
+    else 
+      puts "Invalid detail"
+    end
     
-     if ph_no.match?(/^(\(\d{3}\)\s?|\d{3}-)\d{3}-\d{4}$/)
-       ph_no=ph_no
-     else 
-      puts 'invalid'
-     end
-  end
+   end
+    
+    
 end
