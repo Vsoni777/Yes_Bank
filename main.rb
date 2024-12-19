574cae65b79940041ddd27fc9ebbba528b9da610
Vsoni777 <viveksoni85963@gmail.com>
2024-12-19 16:14:33 +0530

    add super_user dashborad and customer dashborad

--- main.rb
+++ main.rb
@@ -34,85 +34,107 @@ class Main
 
   #Index Page
   def choice_option
+    
       loop do
+      begin
         puts "--------------------------------------------------------------"
-      puts "Enter choice (1 - Create Account, 2 - Login, 3 - Display,4 - exit): "
-      choice=gets.chomp.strip.to_i
-      case choice
-      when 1
-        puts "Create Account and Fill detail #{@@accounts.length}"
-
-        @account=Account.new(@@accounts.length)
-        to_save
-      when 2
-        puts "Enter customer_id"
-        customer_id=gets.chomp.to_i 
-        auth=Checker.login(customer_id,@@accounts)
-        dash_board(auth[:customer_id])
-      when 3
-        puts "account displayed"
-        AccountManager.display(@@accounts)        # to save data on stack
-      when 4
-        puts "exit"
-        # to_save_file
-        puts "--------------------------------------------------------------"
-        break
-      else
-        puts "Invalid choice"
-      end
+        puts "Enter choice (1 - Login, 2- super_user,3 - exit): "
+        choice=gets.chomp.strip.to_i
+       case choice  
+        when 1
+          puts "Enter customer_id"
+           customer_id=gets.chomp.to_i 
+           raise if customer_id<=0
+          auth=Checker.login(customer_id,@@accounts)
+          dash_board(auth[:customer_id])
+        when 2
+          puts "Enter Super user Detail----------------------------"
+          puts "Enter login id:-"
+          raise id=gets.chomp
+          puts "Enter login password:-"
+           password=gets.chomp
+          if Checker.super_user(id,password)
+            AccountManager.dash_board(@@accounts)
+          else
+            puts "Invalid User"
+          end
+        when 3
+          puts "exit"
+          # to_save_file
+          puts "--------------------------------------------------------------"
+          break
+        else
+          puts "Invalid choice"
+       end
+      end    
+    rescue StandardError
+      puts "Enter Valid detail"
+      
+    rescue RuntimeError
+      puts "runtime error"
+      
     end
-  end
+  end  
 
   #Dashboard
   def dash_board(customer_id)
    puts "Welcome in user Dashborad........"
    puts customer_id
+   begin
    loop do
     puts "Enter choice (1 -'Credit',2- 'debit',3 - 'fund transfer',4 -  'display', 5 -'his',6- 'close'): "
    puts "---------------------------------------------------------------"
-   choice=gets.chomp.strip.downcase
-   case choice
-   when "credit"
-    puts "enter amount for Credit"
-    amount=gets.chomp.to_i
-    @account=Operation.credit(customer_id,amount,@@accounts)
-   when "debit"
-    puts "enter amount for Debit"
-    amount=gets.chomp.to_i
-    @account=Operation.debit(customer_id,amount,@@accounts)
-
-   when "transfer"
-    puts "Could you enter second customer _id"
-    # to check first
-    cus_id2=gets.chomp.to_i
-    if Checker.check(cus_id2,@@accounts)
-      puts "enter Amount"
-      amount=gets.chomp.to_i
-      Operation.to_transfer(customer_id,cus_id2,amount,@@accounts)
-    else
-      puts "Invalid is not exist"
+   choice=gets.to_i
+      case choice
+      when 1
+       puts "enter amount for Credit"
+       amount=gets.chomp.to_i
+       raise if amount<=0
+       @account=Operation.credit(customer_id,amount,@@accounts)
+      when 2
+       puts "enter amount for Debit"
+       amount=gets.chomp.to_i
+       raise if amount<=0
+       @account=Operation.debit(customer_id,amount,@@accounts)
+   
+      when 3
+       puts "Could you enter second customer _id"
+       # to check first
+       cus_id2=gets.chomp.to_i
+       if Checker.check(cus_id2,@@accounts)
+         puts "enter Amount"
+         amount=gets.chomp.to_i
+         Operation.to_transfer(customer_id,cus_id2,amount,@@accounts)
+       else
+         puts "Invalid is not exist"
+       end
+      when 4
+       Operation.display(customer_id,@@accounts)
+       # tendo_read_content
+      when 5
+       if Checker.check(customer_id,@@accounts)
+         Operation.display_histroy(customer_id)
+       else
+         puts "worng customer id"
+       end
+      when 6
+       puts "Exit----------------"
+       # to_save_file
+       break
+      else
+       puts "Invalid choice"
+      end
     end
-   when "display"
-    Operation.display(customer_id,@@accounts)
-    # to_read_content
-   when 'his'
-    Operation.display_histroy(@@accounts[customer_id])
-   when "close"
-    puts "Exit----------------"
-    # to_save_file
-    break
-   else
-    puts "Invalid choice"
+   rescue StandardError
+    puts "Invalid Input"
    end
-  end
   end
 
   #convert hash value
-  def to_save 
+  def self.to_save(account) 
     puts account.customer_id
     temp=AccountManager.hash_register(account)
     @@accounts.merge!(temp)
-    puts "#{@@accounts}"
   end
    
   # #to save Data prmanent
