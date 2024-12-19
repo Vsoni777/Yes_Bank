18f9a6bd64a34ed44144eef10c56a2b421bbb5f8
Vsoni777 <viveksoni85963@gmail.com>
2024-12-19 16:20:44 +0530

    handle edge case

--- checker.rb
+++ checker.rb
@@ -1,13 +1,18 @@
+$login_id="yesbank"
+$login_password="rajsir@123"
 module Checker 
    #check Phone_no valid or not
    def self.valid?(ph_no)
     ph_no.match?(/^(\(\d{3}\)\s?|\d{3}-)\d{3}-\d{4}$/)
    end
-
+   
+   def self.super_user(id,password)
+     return $login_id==id && $login_password==password
+   end
    #check login detail for authentication
    #handle exception
    def self.login(customer_id,accounts)
-    return accounts[customer_id]
+         return accounts[customer_id]
    end
 
    def self.check(customer_id,accounts)
