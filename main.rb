3e57eedc2f00b1c5d476224131305f6ccb149b83
Vsoni777 <viveksoni85963@gmail.com>
2024-12-20 13:07:01 +0530

    remove unnessary code and make accounts global

--- main.rb
+++ main.rb
@@ -4,33 +4,31 @@ require_relative 'operation'
 require_relative 'accountmanager'
 require_relative 'dummy'
 require_relative 'tranaction'
-# require './FileSaving'
+require_relative 'authentication'
+require_relative 'customer'
+$histroy_t = Hash.new { |hash, key| hash[key] = [] }
+$login_id="yesbank"
+$login_password="rajsir@123"
+$accounts=Dummy.to_give_dummydata
+
 class Main
   #include module
   include Dummy
   extend AccountManager
   extend Checker
   extend Operation
-  #extend Save
-  @@accounts=Dummy.to_give_dummydata
+  include History
+  include CustomerDashBorad
+  # @@accounts=Dummy.to_give_dummydata
   attr_accessor :account,:operation,:auth,:save
 
   #per Instance call choice_option method
   def initialize
-    History.new
     choice_option
    
   end
 
-  # #to get Account_holder detail
-  # def account_holder
-  #   @account=set
-  # end
-  #check user is authenticate or not
-  def authentication_user(customer_id,name)
-    mathched_detail=Checker.login(customer_id,name,@@accounts)
-    mathched_detail
-  end
+
 
   #Index Page
   def choice_option
@@ -45,8 +43,8 @@ class Main
           puts "Enter customer_id"
            customer_id=gets.chomp.to_i 
            raise if customer_id<=0
-          auth=Checker.login(customer_id,@@accounts)
-          dash_board(auth[:customer_id])
+          auth=Checker.login(customer_id)
+          CustomerDashBorad.dash_board(auth[:customer_id])
         when 2
           puts "Enter Super user Detail----------------------------"
           puts "Enter login id:-"
@@ -54,7 +52,7 @@ class Main
           puts "Enter login password:-"
            password=gets.chomp
           if Checker.super_user(id,password)
-            AccountManager.dash_board(@@accounts)
+            AccountManager.dash_board
           else
             puts "Invalid User"
           end
@@ -68,75 +66,13 @@ class Main
        end
       end        
     rescue Exception
-      puts "runtime error"
+      puts "failed"
       
     end
   end  
 
-  #Dashboard
-  def dash_board(customer_id)
-   puts "Welcome in user Dashborad........"
-   puts customer_id
-   begin
-   loop do
-    puts "Enter choice (1 -'Credit',2- 'debit',3 - 'fund transfer',4 -  'display', 5 -'history',6- 'close'): "
-   puts "---------------------------------------------------------------"
-   choice=gets.to_i
-      case choice
-      when 1
-       puts "enter amount for Credit"
-       amount=gets.chomp.to_i
-       raise if amount<=0
-       @account=Operation.credit(customer_id,amount,@@accounts)
-      when 2
-       puts "enter amount for Debit"
-       amount=gets.chomp.to_i
-       raise if amount<=0
-       @account=Operation.debit(customer_id,amount,@@accounts)
-      when 3
-       puts "Could you enter second customer _id"
-       # to check first
-       cus_id2=gets.chomp.to_i
-       if Checker.check(cus_id2,@@accounts) 
-        if cus_id2!=customer_id
-         puts "enter Amount"
-          amount=gets.chomp.to_i
-          raise if amount<=0
-         Operation.to_transfer(customer_id,cus_id2,amount,@@accounts)
-        else
-          puts "you can't transfer your self"
-        end
-       else
-         puts "Invalid is not exist"
-       end
-      when 4
-       Operation.display(customer_id,@@accounts)
-       # tendo_read_content
-      when 5
-       if Checker.check(customer_id,@@accounts)
-         Operation.display_histroy(customer_id)
-       else
-         puts "worng customer id"
-       end
-      when 6
-       puts "Exit----------------"
-       # to_save_file
-       break
-      else
-       puts "Invalid choice"
-      end
-    end
-   rescue StandardError
-    puts "Invalid Input"
-   end
-  end
+ 
 
-  #convert hash value
-  def self.to_save(account) 
-    puts account.customer_id
-    temp=AccountManager.hash_register(account)
-    @@accounts.merge!(temp)
-  end
 
 end
 
