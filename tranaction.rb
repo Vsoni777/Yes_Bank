0b38098009c7b9ae1a0ea5fe1e7669a61a6d256f
Vsoni777 <viveksoni85963@gmail.com>
2024-12-20 13:07:41 +0530

    remove unnessary code and handle edge case

--- tranaction.rb
+++ tranaction.rb
@@ -1,12 +1,13 @@
-$histroy_t = Hash.new { |hash, key| hash[key] = [] }
-class History
+module History
 
+  #add method to hash set
   def self.add_method(customer, hash_new)
     # Add the new record to the history for the given customer_id
     $histroy_t[customer] << hash_new 
     puts "Record for Customer ID #{customer} added to history."
   end
 
+  #display histor of praticular acc
   def self.display_history(customer)
     # Display the history for a given customer_id
     if $histroy_t.key?(customer)
@@ -23,12 +24,14 @@ class History
     end
   end
 
+  #display clll detail
   def self.display_history_all
-    puts "hi"
     if !$histroy_t.empty?
       $histroy_t.each do |key,detail|
-        puts "--------------------------------------------"
-        puts "customer_Id:      #{key}----------"
+        puts 
+        puts "customer_Id:      #{key}"
+      puts "Operation    "+"  |  "+"     Amount   "+" |  "+"    balance   "+"  |  "+"  time  "+"  |  "+"   |  "+"date"
+      puts "--------------------------------------------"
         detail.each do |details|
        puts "#{details[:operation]}"+"  |  "+"#{details[:amount]}"+"  |  "+"#{details[:balance]}"+"  |  "+" #{details[:time]}  "+" | "+"  #{details[:date]}"
       end
