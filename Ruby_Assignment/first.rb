0afb647c8fdea396438d938200ddce59b903a6ef
Vsoni777 <viveksoni85963@gmail.com>
2024-12-24 16:43:28 +0530

    Convert class

--- Ruby_Assignment/first.rb
+++ Ruby_Assignment/first.rb
@@ -1,30 +1,23 @@
 # Write a Ruby method word_frequency that takes an array of strings as input. Each string contains a sentence. The method should:
 # 1. Count the frequency of each word across all the sentences (case-insensitive).
 # 2. Return a hash where keys are the words, and values are their respective frequencies.
+# hash=Hash.new(0)
+class Frequency
+  attr_accessor :input,:hash
+  def initialize 
+    @hash=Hash.new(0)
+    @input=[
+      "Hello world",
+      "hello Ruby",
+      "Ruby is awesome",
+      "World is big"
+    ]
+  end
 
-sentences = [
-  "Hello world",
-  "hello Ruby",
-  "Ruby is awesome",
-  "World is big"
-]
-new_arr=Array.new
-sentences.each do |value|
- new_arr.push(value.downcase.split)
+  def fre_counter
+  input.join(' ').downcase.split.map!{|word| hash[word]+=1}
+  hash
+  end
 end
 
-new_arr.flatten!
-hash={}
-count=0
-new_arr.each do |element|
-        key=element
-        new_arr.each do |element|
-          if key==element
-            count+=1;
-          end
-        end
-        hash[key]=count
-        count=0
-end
-
-puts hash
+p Frequency.new.fre_counter
