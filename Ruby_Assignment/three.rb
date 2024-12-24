51c00079f64af18fc93f18c1b4a4a8b133ef3f1b
Vsoni777 <viveksoni85963@gmail.com>
2024-12-24 16:44:36 +0530

    convert class

--- Ruby_Assignment/three.rb
+++ Ruby_Assignment/three.rb
@@ -1,14 +1,21 @@
-def count_words(input)
-  word_count = Hash.new(0)
-  
-  def process_string(str, word_count)
+require './first'
+class Finder
+  include Frequency
+  attr_accessor :word_count
+  def initialize
+    @word_count = Hash.new(0)
+  end
+
+  def for_string(str, word_count) 
     str.downcase.split(/\W+/).each { |word| word_count[word] += 1 unless word.empty? }
   end
-  
+
+
   def process(input, word_count)
     case input
-    when String
-      process_string(input, word_count)
+    when String, Symbol
+      input=input.to_s
+      for_string(input,word_count)
     when Hash
       input.each do |key, value|
         process(key, word_count) 
@@ -17,34 +24,40 @@ def count_words(input)
     when Array
       input.each { |element| process(element, word_count) }
     end
-  end
-  
-  process(input, word_count)
-  
-  word_count
+  end 
+
+  def count_words(input)  
+    process(input, word_count)  
+    #comment
+    word_count
+  end 
 end
 
-# input = "Hello world, welcome to Ruby!"
-# input = {
-#   "hello world" => "Ruby is fun",
-#   details: {
-#     description: "Ruby rocks in the programming world",
-#     :"extra info" => ["hello again", "world of Ruby"]
-#   }
-# }
 
-input = [
-  "Hello world",
-  { 
-    "Ruby language" => "is fun",
-    details: [
-      "Ruby rocks", 
-      { "hello again" => "world is round" }
-    ]
-  },
-  [
-    "Complex structures are interesting",
-    { more_data: ["Hello", "world of Ruby"] }
-  ]
-]
-puts count_words(input)
+
+
+
+
+# input = "Hello world, welcome to Ruby!"
+input = {
+  "hello world" => "Ruby is fun",
+  details: {
+    description: "Ruby rocks in the programming world",
+    :"extra info" => ["hello again", "world of Ruby"]
+  }
+}
+ p Finder.new.count_words(input)
+# input = [
+#   "Hello world",
+#   { 
+#     "Ruby language" => "is fun",
+#     details: [
+#       "Ruby rocks", 
+#       { "hello again" => "world is round" }
+#     ]
+#   },
+#   [
+#     "Complex structures are interesting",
+#     { more_data: ["Hello", "world of Ruby"] }
+#   ]
+# ]
